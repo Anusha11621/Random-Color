d5e13d3dc761be432b25e2eff570f1ec2c11c0cf
Anusha <76464459+Anusha11621@users.noreply.github.com>
2022-12-14 11:18:49 +0530

    added heading

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ let body = document.querySelector('body')
 let container = document.createElement('div')
 container.classList.add('container')
 body.append(container)
+let headingdiv = document.createElement('div')
+headingdiv.classList.add('heading')
+container.append(headingdiv)
+let h1 = document.createElement('h1')
+h1.textContent = "🤩 Disco Grid 🤩 "
+headingdiv.append(h1)
 
 for (let i = 1;i<=500;i++){
     let box = document.createElement('div')
     box.classList.add('box')
-    box.textContent = i
+    box.textContent = Math.floor(Math.random()*500)
     container.append(box)
 }
-console.log(body);
+
 document.addEventListener("mouseover",function(){
     let click = document.querySelectorAll('.box')
    
     for(let j=0; j<click.length;j++){
         click[j].textContent = Math.floor(Math.random()*500);
-        click[j].style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16)
+        click[j].style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16) 
         // click.style.backgroundColor = "blue"
     }
 })
